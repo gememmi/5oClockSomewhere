9f2b57c8b16020353a5d6bedc681d11502009362
Emily <emelizadaniels@gmail.com>
2022-10-24 16:58:17 -0400

    started click menu event listener and created image array

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const URL = "http//api.openbrewerydb.org/breweries?by_city=new_york"
+// var item = items[Math.floor(Math.random()*items.length)];
 
+const imageArray = [
+    "/Users/emilydaniels/Development/code/phase-1-project/images/alphabet-city-beer-company.jpeg",
+ "/Users/emilydaniels/Development/code/phase-1-project/images/b9 beverages.webp",
+    "/Users/emilydaniels/Development/code/phase-1-project/images/birreriaEataly.jpeg",
+    "/Users/emilydaniels/Development/code/phase-1-project/images/deathave.jpeg",
+    "/Users/emilydaniels/Development/code/phase-1-project/images/empire city .jpeg",
+    "/Users/emilydaniels/Development/code/phase-1-project/images/harlembluebeer.jpg",
+  "/Users/emilydaniels/Development/code/phase-1-project/images/harlembrewery.jpeg", "/Users/emilydaniels/Development/code/phase-1-project/images/heartland.jpg", "/Users/emilydaniels/Development/code/phase-1-project/images/owlsBrew.jpeg","/Users/emilydaniels/Development/code/phase-1-project/images/radiantPig.webp","/Users/emilydaniels/Development/code/phase-1-project/images/ruckusco.jpeg","/Users/emilydaniels/Development/code/phase-1-project/images/ruckusco.jpeg", "/Users/emilydaniels/Development/code/phase-1-project/images/thirdRail.jpeg", "/Users/emilydaniels/Development/code/phase-1-project/images/toastAle.jpeg"
+]
 fetch("https://api.openbrewerydb.org/breweries?by_city=new_york")
     .then(response => response.json())
     .then (data => renderData(data))
@@ -7,9 +17,21 @@ fetch("https://api.openbrewerydb.org/breweries?by_city=new_york")
 function renderData(data){
     console.log(data)
     data.forEach(data => {
-        let breweryLi = document.createElement('li')
+        let breweryLi = document.createElement('h5')
         breweryLi.textContent = data.name
-        console.log(breweryLi) 
-        document.querySelector('#brewery-list').append(breweryLi)       
+        document.querySelector('#brewery-list').append(breweryLi)
+        
+    
+        breweryLi.addEventListener('click', (e) => {
+            beerNameDetail = document.querySelector('#brewery-detail-name')
+            beerNameDetail.textContent = data.name
+
+        // let randomImage = imageArray[Math.floor(Math.random()*imageArray.length)];
+        // detailImage = document.querySelector('#beer-detail-image')
+        // detailImage.src = randomImage
+            
+        }
+        )      
     })
 }
+
