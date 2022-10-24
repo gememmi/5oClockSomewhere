2d7a5ba27d491a7874e771fbd740c4eb92b9bc7f
Abena Adu <abeanadu@gmail.com>
2022-10-24 16:27:19 -0400

    Selecting Elements for JS

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
 const URL = "http//api.openbrewerydb.org/breweries?by_city=new_york"
 
 fetch("https://api.openbrewerydb.org/breweries?by_city=new_york")
     .then(response => response.json())
     .then (data => renderData(data))
+
+const breweryImage = document.querySelector('#beer-detail-image')
+const breweryName = document.querySelector('#brewery-detail-name')
+const breweryType = document.querySelector('#brewery-detail-type')
+const breweryPhoneNumber = document.querySelector('#')
 
 function renderData(data){
     console.log(data)
@@ -10,6 +16,9 @@ function renderData(data){
         let breweryLi = document.createElement('li')
         breweryLi.textContent = data.name
         console.log(breweryLi) 
-        document.querySelector('#brewery-list').append(breweryLi)       
+        document.querySelector('#brewery-list').append(breweryLi) 
+        breweryLi.addEventListener('click', e => {
+            
+        })      
     })
 }
