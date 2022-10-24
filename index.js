01bd389fd38a7086282cc8d135d096af3903bb15
Emily <emelizadaniels@gmail.com>
2022-10-24 15:33:14 -0400

    changed ul, fetched data

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const URL = https://api.openbrewerydb.org/breweries?by_city=new_york
+const URL = "http//api.openbrewerydb.org/breweries?by_city=new_york"
 
-function onload() {
 fetch("https://api.openbrewerydb.org/breweries?by_city=new_york")
-.then(response => response.json())
-.then (data => console.log(data))
+    .then(response => response.json())
+    .then (data => renderData(data))
+
+function renderData(data){
+    console.log(data)
+    data.forEach(data => {
+        let breweryLi = document.createElement('li')
+        breweryLi.textContent = data.name
+        console.log(breweryLi) 
+        document.querySelector('#brewery-list').append(breweryLi)       
+    })
 }
